37651159624b17e549d261b1b7fca352eb4df101
yang-zhang <zhangyangcolostate@gmail.com>
2017-12-15 16:25:27 -0500

    Update

--- deeplearning2/vgg16_avg_exp.py
+++ deeplearning2/vgg16_avg_exp.py
@@ -35,8 +35,7 @@ def VGG16_Avg(include_top=True, weights='imagenet',
                                       default_size=224,
                                       min_size=48,
                                       data_format=K.image_dim_ordering(),
-                                      #include_top=include_top,
-                                      require_flatten=False,
+                                      require_flatten=include_top,
                                      )
 
     if input_tensor is None:
